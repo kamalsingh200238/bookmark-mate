82ab704ef2c75fce2010738f700e935f3ff3423e
Kamal Singh <kamalsingh200238@gmail.com>
2023-04-08 16:13:06 +0530

    feat(server): Setup Basic Server To Check If Everything Is Working

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT ?? 3333; // if port not present in .env file then run on 3333
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server which is not typescript yet😛');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
