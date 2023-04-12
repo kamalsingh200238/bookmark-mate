040264f62bc327299b058682ccc63df7e39d754d
Kamal Singh <kamalsingh200238@gmail.com>
2023-04-12 23:05:21 +0530

    feat(server): Add route and refactor file               - First refactored the whole file                   - Created a start function that handles everything                     related to starting a server.               - Second added bookmark route at /api/v1/bookmarks/

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,40 +1,36 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-// routes
-// import mainRoutes from '../routes/main.js'  //error here
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import bookmarks from "./routes/bookmarks";
 
 dotenv.config(); // setting up dotenv to use .env varaible
 
 const app = express();
 const port = process.env.PORT ?? 3333; // if port not present in .env file then run on 3333
 
-app.use(express.json()) // This will parse JSON data in incoming requests
+// middlewares
+app.use(express.json()); // This will parse JSON data in incoming requests
 
-/* ==============================================
-========== Database Setup
-================================================*/
-
-const db = process.env.DB_STRING as string  //set db to the DB_STRING from the env file
-
-// connect to database
-mongoose.connect(db)
-  .then(() => console.log("MongoDB is connected"))
-  .catch((error) => console.log("There was an error connecting to MongoDB",error));
-
-/* ==============================================
-========== Database Setup End
-================================================*/
-
-app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server which is typescript Now. ');
+app.get("/", (_req, res) => {
+  res.send("Express + TypeScript Server which is typescript Now. ");
 });
 
+// routes
+app.use("/api/v1/bookmarks/", bookmarks);
 
-// app.use("/", mainRoutes);
+async function start() {
+  const db = process.env.DB_STRING as string; //set db to the DB_STRING from the env file
+  // connect to database
+  await mongoose
+    .connect(db)
+    .then(() => console.log("MongoDB is connected"))
+    .catch((error) =>
+      console.log("There was an error connecting to MongoDB", error)
+    );
 
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+}
 
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+start();
