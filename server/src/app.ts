437fd5436484b345620a6efadd70de534f42bd4f
Kamal Singh <kamalsingh200238@gmail.com>
2023-04-12 20:26:14 +0530

    chore(server): comment everything that is not working

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,25 +2,25 @@ import express from 'express';
 import { config } from 'dotenv';
 import mongoose from 'mongoose';
 // routes
-import mainRoutes from '../routes/main.js'  //error here
+// import mainRoutes from '../routes/main.js'  //error here
 
-config();
+config(); // setting up config to use .env varaible
 
 const app = express();
 const port = process.env.PORT ?? 3333; // if port not present in .env file then run on 3333
-const db = process.env.DB_STRING as string  //set db to the DB_STRING from the env file
+// const db = process.env.DB_STRING as string  //set db to the DB_STRING from the env file
 
 // connect database
-mongoose.connect(db)
-  .then(() => console.log("DB is connected"))
-  .catch((error) => console.log(error));
+// mongoose.connect(db)
+//   .then(() => console.log("DB is connected"))
+//   .catch((error) => console.log(error));
 
 app.get('/', (req, res) => {
   res.send('Express + TypeScript Server which is typescript Now. ');
 });
 
 
-app.use("/", mainRoutes);
+// app.use("/", mainRoutes);
 
 
 
