1f4f9b6926f7215c148a73ca99704d1fc0e64c65
Kamal Singh <kamalsingh200238@gmail.com>
2023-04-12 23:09:45 +0530

    fix(server): Delete javascript files.

--- server/routes/main.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const router = express.Router();
-const mainController=require("../controllers/main")
-
-router.get("/", mainController.getTest);
-router.post("/bookmark",mainController.addToBookmark);
-
-module.exports = router;
