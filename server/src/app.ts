413e988278daac120236afdbc39bd1e0694b141d
Kamal Singh <101921006+kamalsingh200238@users.noreply.github.com>
2023-04-13 21:56:33 +0530

    Merging after creating mvc structure. (#5)
    * added models,controllers and routes folders
    
    * added mvc structure,a user model and connected the database
    
    * commented user model
    
    * added main router
    
    * created a main controller and a BookMark model
    
    * changed bookmark model schma name to BookmarkSchema
    
    * added addToBookmark in main controller, also commented the error
    
    * Fix typescript errors (#4)
    
    * fix: Rename index.ts to app.ts (it makes more sense, following a convention).
    
    * chore(server): comment everything that is not working
    
    * chore(server): changed config() to dotenv.config(), cause it makes more sense
    
    * refactor(server): Refactor Database Setup
    
    * feat(server): Add body parser middleware
    
    * feat(server): Create Bookmark model
    
    * feat(server): Create User model
    
    * feat(server): Create bookmark route
    
    * feat(server): Add route and refactor file
                  - First refactored the whole file
                      - Created a start function that handles everything
                        related to starting a server.
                  - Second added bookmark route at /api/v1/bookmarks/
    
    * fix(server): Delete javascript files.
    
    * feat(server/controllers): Add bookmark controller.
    
    * chore(server): Add prettier for consistent formatting\
    
    * format(server): Format server folder
    
    ---------
    
    Co-authored-by: Reem <reembsrat@gmail.com>

---
+++ server/src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import bookmarks from './routes/bookmarks';
+
+dotenv.config(); // setting up dotenv to use .env varaible
+
+const app = express();
+const port = process.env.PORT ?? 3333; // if port not present in .env file then run on 3333
+
+// middlewares
+app.use(express.json()); // This will parse JSON data in incoming requests
+
+app.get('/', (_req, res) => {
+  res.send('Express + TypeScript Server which is typescript Now. ');
+});
+
+// routes
+app.use('/api/v1/bookmarks/', bookmarks);
+
+async function start() {
+  const db = process.env.DB_STRING as string; //set db to the DB_STRING from the env file
+  // connect to database
+  await mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB is connected'))
+    .catch((error) =>
+      console.log('There was an error connecting to MongoDB', error)
+    );
+
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+}
+
+start();
---
+++ server/src/models/User.ts
@@ -0,0 +1,40 @@
+import { Schema, model } from 'mongoose';
+
+export interface IUser {
+  username: String;
+  email: String;
+  password: String;
+}
+
+// A user schema with a username,email and password
+const UserSchema = new Schema<IUser>(
+  {
+    // username is a string,with a minimum of 4 characters,all usernames should be unique,and its required of all users
+    username: {
+      type: String,
+      min: 4,
+      unique: true,
+      required: true,
+    },
+
+    // email is a string,with a minimum of 4 characters and is reqired for all users
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    // password is a string,with a minimum of 8 characters and is required for all users
+    password: {
+      type: String,
+      min: 8,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model<IUser>('User', UserSchema);
+export default User;
