71211cc976c8e2feb133a409c7e8db5a324750b0
jackie <jm20wd@gmail.com>
2023-04-25 21:58:27 +0100

    Rebase with main branch

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import bookmarks from './routes/bookmarks';
-<<<<<<< HEAD
-import auth from './routes/auth';
+import auth from './routes/auth'; 
 import cookieParser from 'cookie-parser';
-=======
 import { findBookmark, deleteBookmark } from './controllers/bookmarks';
->>>>>>> e297b24 (Add delete bookmark and get bookmark routes)
 
 dotenv.config(); // setting up dotenv to use .env varaible
 
@@ -24,13 +21,10 @@ app.get('/', (_req, res) => {
 
 // routes
 app.use('/api/v1/bookmarks/', bookmarks);
-<<<<<<< HEAD
 app.use('/api/v1/auth/', auth);
 
-=======
 app.get('/api/v1/:id/find', findBookmark)
-app.delete('/api/v1/:id/delete', deleteBookmark)
->>>>>>> e297b24 (Add delete bookmark and get bookmark routes)
+app.delete('/api/v1/:id/delete', deleteBookmark) 
 
 async function start() {
   const db = process.env.DB_STRING as string; //set db to the DB_STRING from the env file
