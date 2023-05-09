eaa1d5e778d694cc206eae43bc0133786f92d55e
Phillip Elliott <89685937+Phil-Elliott@users.noreply.github.com>
2023-05-09 13:37:07 +0700

    Generate JWT token and set as a cookie in Register route (#11)

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,7 +5,9 @@ import bookmarks from './routes/bookmarks';
 import auth from './routes/auth';
 import cookieParser from 'cookie-parser';
 
-dotenv.config(); // setting up dotenv to use .env varaible
+dotenv.config(); // setting up dotenv to use .env variable
+
+console.log('process.env.DB_STRING', process.env.DB_STRING);
 
 const app = express();
 const port = process.env.PORT ?? 3333; // if port not present in .env file then run on 3333
