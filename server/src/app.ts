6182c45ea6d38bf36dfd285b1c603fc50b30fa85
Kamal Singh <kamalsingh200238@gmail.com>
2023-05-27 12:07:48 +0530

    refactor(app.ts): refactor app.ts file
    - Fixed spacing
    - Fixed formatting
    - Removed console.logs
    - Add good comments

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,31 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import bookmarks from './routes/bookmarks';
-import auth from './routes/auth';
 import cookieParser from 'cookie-parser';
-import cors from "cors"
+import cors from 'cors';
+import mongoose from 'mongoose';
+import auth from './routes/auth';
+import bookmarks from './routes/bookmarks';
 
 dotenv.config(); // setting up dotenv to use .env variable
-
-console.log('process.env.DB_STRING', process.env.DB_STRING);
-
 const app = express();
 const port = process.env.PORT ?? 3333; // if port not present in .env file then run on 3333
-
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-
-// middlewares
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' })); // enable cors
 app.use(express.json()); // This will parse JSON data in incoming requests
-app.use(cookieParser());
+app.use(cookieParser()); // This will parse cookies in incoming requests
 
 app.get('/', (_req, res) => {
-  res.send('Express + TypeScript Server which is typescript Now. ');
+  res.send('This is the / route');
 });
 
 // routes
-app.use('/api/v1/bookmarks/', bookmarks);
 app.use('/api/v1/auth/', auth);
+app.use('/api/v1/bookmarks/', bookmarks);
 
 async function start() {
   const db = process.env.DB_STRING as string; //set db to the DB_STRING from the env file
