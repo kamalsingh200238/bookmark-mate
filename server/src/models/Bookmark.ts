e48910cc01f37ef076d7ac35ec999efc3c6db69e
Kamal Singh <kamalsingh200238@gmail.com>
2023-05-28 20:11:04 +0530

    feat(models/Bookmark.ts): Add user in schema - Add user in schema - Removed IUser with UserDocument

--- server/src/models/Bookmark.ts
+++ server/src/models/Bookmark.ts
@@ -1,29 +1,33 @@
-import { Schema, model } from 'mongoose';
+import { ObjectId, Schema, model } from 'mongoose';
 
-export interface IBookmark {
+export interface BookmarkDocument extends Document {
+  user: ObjectId;
   nameOfBookmark: string;
   urlOfBookmark: string;
 }
 
 // Bookmark Schema with nameofBookMark, urlOfBookMark
-const BookmarkSchema = new Schema<IBookmark>(
+const BookmarkSchema = new Schema<BookmarkDocument>(
   {
     // nameofBookMark is a string and its required
     nameOfBookmark: {
       type: String,
       required: true,
     },
-    // urlOfBookMark is a string and is reqired
 
+    // urlOfBookMark is a string and is reqired
     urlOfBookmark: {
       type: String,
       required: true,
     },
+
+    // id of the user who created the bookmark
+    user: { type: Schema.Types.ObjectId, required: true },
   },
   {
     timestamps: true,
   }
 );
 
-const Bookmark = model('Bookmark', BookmarkSchema);
+const Bookmark = model<BookmarkDocument>('Bookmark', BookmarkSchema);
 export default Bookmark;
